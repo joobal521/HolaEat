49bba81898677ede32e0a314a89ccd27d9a6ef8e
joobal521 <perbell521@gmail.com>
2023-08-05 19:08:05 +0900

    first commit

---
+++ src/main/java/com/spring/holaeat/HolaEatApplication.java
@@ -0,0 +1,13 @@
+package com.spring.holaeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/holaeat/HolaEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.holaeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
