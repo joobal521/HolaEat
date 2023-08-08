0c6cf1c1ddd7e1a61b2b02f014e80a54ef67e26c
joobal521 <perbell521@gmail.com>
2023-08-08 14:18:48 +0900

    UserRepository 오류 해결2

--- src/main/java/com/spring/holaeat/domain/user/UserRepository.java
+++ src/main/java/com/spring/holaeat/domain/user/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
 //    // 수정된 메소드명과 반환 타입
-//    public List<User> findAllByUserIdAndUserEmail(String userId, String userEmail);
+  public List<User> findAllByUserIdAndUserEmail(String userId, String userEmail);
 //
 //    //2
 //    //SELECT *FROM users WHERE email LIKE ?
