b6a1ec0fcf120e0046040a2b9454c53b20764848
joobal521 <perbell521@gmail.com>
2023-08-16 23:41:48 +0900

    email ing...1

--- src/main/java/com/spring/holaeat/controller/LogController.java
+++ src/main/java/com/spring/holaeat/controller/LogController.java
@@ -40,7 +40,7 @@ public class LogController {
         if (user != null) {
             if (user.getUserPassword().equals(userPassword)) {
                 UserDetail userDetail = userDetailRepository.findByUserId(userId);
-                ProfileImg profileImg=profileImgRepository.findByUser(user);
+                ProfileImg profileImg=profileImgRepository.findByUserId(userId);
 
                 if(profileImg!=null){
                     session.setAttribute("profileImg", profileImg.getProfileImg());
