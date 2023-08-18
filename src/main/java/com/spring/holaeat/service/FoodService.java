eae239c828833d70c04d6db4bd13ffa336e3cbb2
dongtuta <sourcefilmer@gmail.com>
2023-08-18 11:57:17 +0900

    adminMenu added filter

--- src/main/java/com/spring/holaeat/service/FoodService.java
+++ src/main/java/com/spring/holaeat/service/FoodService.java
@@ -30,20 +30,39 @@ public class FoodService {
     }
 
 
-        public void addFood(FoodRequestDto foodRequestDto){
-//            String id = generateId();
-            Food food = new Food(
-//                    id,
-                    foodRequestDto); // Food 생성자 수정 필요
-            foodRepository.save(food);
+
+    public String generateNewFoodId() {
+        // 현재 데이터베이스에 저장되어 있는 모든 foodId를 가져옵니다.
+        List<String> existingFoodIds = foodRepository.getAllFoodIds();
+
+        // 새로운 foodId를 생성할 숫자를 찾습니다.
+        int nextNumber = 1;
+        for (String existingFoodId : existingFoodIds) {
+            String numberPart = existingFoodId.substring(1); // F 다음의 숫자 부분
+            int existingNumber = Integer.parseInt(numberPart);
+            if (existingNumber >= nextNumber) {
+                nextNumber = existingNumber + 1;
+            }
         }
 
-//        private String generateId() {
-//            // 데이터베이스에서 가장 큰 ID 값 조회 후 +1
-//            int maxId = foodRepository.findMaxId(); // foodRepository에서 해당 메소드를 구현해야 함
-//            return "F" + String.format("%04d", maxId + 1); // "F0055", "F0056", ...
-//        }
+        // 생성된 숫자를 4자리로 만들어줍니다.
+        String formattedNumber = String.format("%04d", nextNumber);
 
+        // 최종적으로 생성된 foodId를 반환합니다.
+        return "F" + formattedNumber;
+    }
+
+    public void addFood(FoodRequestDto foodRequestDto) {
+        String newFoodId = generateNewFoodId();
+        System.out.println(newFoodId);
+        foodRequestDto.setFoodId(newFoodId);
+
+
+        Food food = new Food(foodRequestDto);
+        foodRepository.save(food);
+        // 이후에 데이터를 저장하고 관련 처리를 수행합니다.
+        // ...
+    }
 
     public List<Food> findFoodListByFoodId(String foodId){
         return foodRepository.findFoodListByFoodId(foodId);
