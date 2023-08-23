5c56a44c32daac2ae9a6fac61bf49e352c1997ac
joobal521 <perbell521@gmail.com>
2023-08-23 19:45:36 +0900

    index.css

--- src/main/java/com/spring/holaeat/HolaEatApplication.java
+++ src/main/java/com/spring/holaeat/HolaEatApplication.java
@@ -2,11 +2,17 @@ package com.spring.holaeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class HolaEatApplication {
+public class HolaEatApplication extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(HolaEatApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HolaEatApplication.class, args);
